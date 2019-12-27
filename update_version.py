d7a4adbd1b0f6cd705bc8de47a285e8114b6f31c
gkovacs81 <gkovacs81@gmail.com>
2019-12-27 14:48:22 +0100

    Increase version + formatting

--- update_version.py
+++ update_version.py
@@ -1,31 +1,32 @@
-
 import fileinput
-import subprocess
 import sys
 
 from utils import get_repository_version
 
-__version__ = "V0.1"
+__version__ = "V0.3.00"
 
 
 def replace(filename, placeholder, value):
     with fileinput.FileInput(filename, inplace=True) as file:
         for line in file:
-            print(line.replace(placeholder, value), end='')
+            print(line.replace(placeholder, value), end="")
 
 
 def main():
     server_version = __version__ + ":" + get_repository_version("server")
-    webapplication_version = __version__ + ':' + get_repository_version("webapplication")
+    webapplication_version = (
+        __version__ + ":" + get_repository_version("webapplication")
+    )
 
     print("Server: %s" % server_version)
     print("Webapp: %s" % webapplication_version)
 
-    with open('server/src/server/version.py', 'w') as f:
+    with open("server/src/server/version.py", "w") as f:
         f.write('__version__="{}"'.format(server_version))
 
-    with open('webapplication/src/app/version.ts', 'w') as f:
+    with open("webapplication/src/app/version.ts", "w") as f:
         f.write('export const VERSION = "{}"'.format(webapplication_version))
+
 
 if __name__ == "__main__":
     sys.exit(main())
