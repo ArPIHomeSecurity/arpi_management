d9f403384b54a786c921f020f1072abbfddae898
gkovacs81 <gkovacs81@gmail.com>
2020-12-02 08:46:30 +0100

    Upgrade letsencrypt install

--- scripts/install_environment.sh
+++ scripts/install_environment.sh
@@ -34,24 +34,23 @@ fi
 
 # DATABASE
 printf "\n\n# Database install"
-echo "# Install postgres"
-sudo apt-get install -y postgresql
-echo "# Configure access"
+echo "## Install postgres"
+sudo apt-get install -y postgresql postgresql-server-dev-all
+echo "## Configure access"
 sudo su -c "psql -c \"CREATE USER $ARGUS_DB_USERNAME WITH PASSWORD '$ARGUS_DB_PASSWORD';\"" postgres
-echo "# Create database"
+echo "## Create database"
 sudo su -c "createdb -E UTF8 -e $ARGUS_DB_SCHEMA" postgres
 
 # CERTBOT
 printf "\n\n# Install certbot"
-wget https://dl.eff.org/certbot-auto
-sudo mv certbot-auto /usr/local/bin/certbot-auto
-sudo chown root /usr/local/bin/certbot-auto
-sudo chmod 0755 /usr/local/bin/certbot-auto
-# Remove pip config to avoid sha256 mismatch
-# https://community.letsencrypt.org/t/certbot-auto-certificates-fails-while-installing-phyton-packages-with-these-packages-do-not-match-the-hashes/90363/6
-sudo /usr/local/bin/certbot-auto --noninteractive
-sudo mv /etc/pip.conf /etc/pip.conf_backup
-sudo /usr/local/bin/certbot-auto --noninteractive
+printf "## Install snapd"
+sudo apt-get -y install snapd
+printf "## Update snapd"
+sudo snap install core; sudo snap refresh core
+printf "## Update certbot snapd package"
+sudo snap install certbot --classic
+printf "## Prepare the command"
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 # RTC
 # based on https://www.abelectronics.co.uk/kb/article/30/rtc-pi-on-raspbian-buster-and-stretch
